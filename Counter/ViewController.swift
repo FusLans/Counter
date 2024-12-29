e617289c16aac27bea830288d8e4fe06098db3bf
FusLans <neikomaxi@gmail.com>
2024-12-29 20:46:25 +0300

    Осталось добавить скролл

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     let date = Date()
     @IBOutlet weak var countLable: UILabel!
     @IBOutlet weak var plus: UIButton!
-   
+    
     var Counter: Int = 0
     @IBOutlet weak var zero: UIButton!
     @IBOutlet weak var minus: UIButton!
@@ -22,26 +22,30 @@ class ViewController: UIViewController {
         plus.tintColor = .red
         zero.tintColor = .green
         countLable.text = String(Counter)
-        
-        
-    }
-    @IBAction private func plusButton(_ sender: Any) {
-       Counter += 1
-       countLable.text = String(Counter)
-        
     }
     
+  
+    @IBAction private func plusButton(_ sender: Any) {
+        Counter += 1
+        countLable.text = String(Counter)
+        history.text += "\(date): значение изменено на +1 \n"
+    }
+
     @IBAction private func zeroButton(_ sender: Any) {
         Counter = 0
         countLable.text = String(Counter)
+        history.text += "\(date): значение сброшено \n"
     }
     @IBAction private func minusButton(_ sender: Any) {
         if Counter > 0{
             Counter -= 1
+            history.text += "\(date): значение изменено на -1 \n "
         } else {
+            history.text += "\(date): попытка уменьшить значение счетчика ниже 0 \n"
             Counter = 0
         }
         countLable.text = String(Counter)
     }
+    
+    
 }
-
