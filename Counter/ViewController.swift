dc4586577eb6cff72cfb78999cd08d4816c476f7
FusLans <neikomaxi@gmail.com>
2024-12-29 18:59:14 +0300

    Осталось добавить поле изменений

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -4,16 +4,44 @@
 //
 //  Created by Максим Ботин on 29.12.2024.
 //
-
+import Foundation
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var history: UITextView!
+    let date = Date()
+    @IBOutlet weak var countLable: UILabel!
+    @IBOutlet weak var plus: UIButton!
+   
+    var Counter: Int = 0
+    @IBOutlet weak var zero: UIButton!
+    @IBOutlet weak var minus: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        plus.tintColor = .red
+        zero.tintColor = .green
+        countLable.text = String(Counter)
+        
+        
     }
-
-
+    @IBAction private func plusButton(_ sender: Any) {
+       Counter += 1
+       countLable.text = String(Counter)
+        
+    }
+    
+    @IBAction private func zeroButton(_ sender: Any) {
+        Counter = 0
+        countLable.text = String(Counter)
+    }
+    @IBAction private func minusButton(_ sender: Any) {
+        if Counter > 0{
+            Counter -= 1
+        } else {
+            Counter = 0
+        }
+        countLable.text = String(Counter)
+    }
 }
 
